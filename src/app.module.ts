5f56de5077fa3515b400b0ac638a07410c6fdb07
jongunee <jejeoppa@naver.com>
2022-09-08 10:01:19 +0900

    k8s service, controller added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { K8sController } from './k8s/k8s.controller';
+import { K8sService } from './k8s/k8s.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, K8sController],
+  providers: [AppService, K8sService],
 })
 export class AppModule {}
---
+++ src/k8s/k8s.controller.ts
@@ -0,0 +1,4 @@
+import { Controller } from '@nestjs/common';
+
+@Controller('k8s')
+export class K8sController {}
